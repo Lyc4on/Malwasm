2f8a1f017334d285b28e51da369c4efbc14fdb46
Yang Jun <2000572@sit.singaporetech.edu.sg>
2022-04-01 11:40:56 +0800

    detection v2 with improved detection rate

--- malwasm_detection.py
+++ malwasm_detection.py
@@ -5,10 +5,11 @@ import time
 import urllib
 import pyfiglet
 import shutil
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from pathlib import Path
+
 
 # Input URL of website as an argument
 parser = argparse.ArgumentParser()
@@ -35,26 +36,27 @@ print("Detecting WASM files...")
 driver = webdriver.Chrome(service=ser, options=chrome_options)
 # Navigate to the specified URL
 driver.get(args.URL)
-# Wait for 5 seconds for the network requests to be made
-time.sleep(5)
-# Get output of all network requests made
-timings = driver.execute_script("return window.performance.getEntries();")
-# Close all windows and end driver's process
-driver.quit()
 
 # Get current working directory
 current_working_directory = os.getcwd() + "\\Temp\\"
-print("WASM files found:")
 wasm_arr = []
-for i in range(len(timings)):
-    # Find .wasm files
-    if ".wasm" in timings[i]['name']:
-        print(timings[i]['name'])
-        filename = os.path.basename(timings[i]['name'])
-        full_filename = os.path.join(current_working_directory, filename)
-        wasm_arr.append(full_filename)
-        # Save the .wasm file into Temp folder
-        urllib.request.urlretrieve(timings[i]['name'], filename=full_filename)
+
+# Wait for 5 seconds for the network requests to be made
+time.sleep(5)
+# Access requests via the `requests` attribute
+print("WASM files found:")
+for request in driver.requests:
+    if request.response:
+        if request.response.headers['Content-Type'] == 'application/wasm':
+            print(request.url)
+            filename = os.path.basename(request.url)
+            full_filename = os.path.join(current_working_directory, filename)
+            wasm_arr.append(full_filename)
+            # Save the .wasm file into Temp folder
+            urllib.request.urlretrieve(request.url, filename=full_filename)
+
+# Close all windows and end driver's process
+driver.quit()
 
 if len(wasm_arr) == 0:
     print("No WASM files detected!")
@@ -63,3 +65,25 @@ print("\nPerforming analysis...")
 
 # Clean up by deleting Temp folder
 shutil.rmtree(current_working_directory)
+
+
+# # Get output of all network requests made
+# timings = driver.execute_script("return window.performance.getEntries();")
+# print(len(timings))
+# # Close all windows and end driver's process
+# driver.quit()
+#
+# # Get current working directory
+# current_working_directory = os.getcwd() + "\\Temp\\"
+# print("WASM files found:")
+# wasm_arr = []
+# for i in range(len(timings)):
+#     # Find .wasm files
+#     if ".wasm" in timings[i]['name']:
+#         print(timings[i]['name'])
+#         filename = os.path.basename(timings[i]['name'])
+#         full_filename = os.path.join(current_working_directory, filename)
+#         wasm_arr.append(full_filename)
+#         # Save the .wasm file into Temp folder
+#         urllib.request.urlretrieve(timings[i]['name'], filename=full_filename)
+#
